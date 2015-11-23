70fc22812cfe7779040fc05480b27c25adc41cd9
Erick <erick.rhen@ndsu.edu>
2015-11-23 14:27:09 -0600

    Small changes to table

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,21 +14,8 @@ namespace Server
         {
             
             DatabaseWorker test = new DatabaseWorker();
+            test.ClearFriendsTable();
             test.ClearUsersTable();
-            User user1 = new User();
-            User user2 = new User();
-            user1.Username = "HEY";
-            user1.Password = "HELLO";
-            user2.Username = "Mac";
-            test.AddUser(user1);
-            
-            test.AddFriend(user1, user2);
-            test.getFriendsList(user1.Username);
-            List<string> temp = test.getFriendsList(user1.Username);
-            foreach (var a in temp)
-            {
-                Console.WriteLine(a);
-            }
         }
     }
 
