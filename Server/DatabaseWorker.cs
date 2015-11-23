3c327260190b3db27f0270d211a1b45f91bd0be8
Erick <erick.rhen@ndsu.edu>
2015-11-23 14:19:51 -0600

    Got some more querries working.  Nearly ready to get listeners setup for communication

--- Server/DatabaseWorker.cs
+++ Server/DatabaseWorker.cs
@@ -24,7 +24,7 @@ namespace Server
         //public string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Erick\\Source\\Repos\\Communication-System\\Server\\UserDatabase.mdf;Integrated Security=True;Connect Timeout=30";
         public string conString;
 
-        public void Insert(User user)
+        public void AddUser(User user)
         {
             try
             {
@@ -79,6 +79,28 @@ namespace Server
                     command.Parameters["@Username"].Value = user.Username;
 
                     command.Parameters.Add("@FriendUsername", SqlDbType.NVarChar, 50);
+                    command.Parameters["@FriendUsername"].Value = friend.Username;
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Friends (Username, FriendUsername) VALUES "
+                        + "(@Username, @FriendUsername)", connection);
+
+                    command.Parameters.Add("@Username", SqlDbType.NVarChar, 50);
+                    command.Parameters["@Username"].Value = friend.Username;
+
+                    command.Parameters.Add("@FriendUsername", SqlDbType.NVarChar, 50);
                     command.Parameters["@FriendUsername"].Value = user.Username;
 
                     command.ExecuteNonQuery();
@@ -127,6 +149,153 @@ namespace Server
             catch (Exception e)
             {
 
+            }
+        }
+
+        public List<string> getUsernameInfo(string username)
+        {
+            List<string> values = new List<string>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE Username = @Username", connection);
+
+                    command.Parameters.Add("@Username", SqlDbType.NVarChar, 50);
+                    command.Parameters["@Username"].Value = username;
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    int numberOfColumns = getNumberOfColumnsUsers();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < numberOfColumns; i++)
+                            {
+                                values.Add(reader[i].ToString());
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        // Always call Close when done reading.
+                        reader.Close();
+
+                    }
+                    return values;
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                return values;
+            }
+        }
+
+        public int getNumberOfColumnsUsers()
+        {
+            int columns = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT Count(*) FROM information_schema.columns WHERE TABLE_NAME = 'Users'", connection);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            columns = Int32.Parse(reader[0].ToString());
+                        }
+                    }
+                    finally
+                    {
+                        // Always call Close when done reading.
+                        reader.Close();
+                    }
+                }
+                return columns;
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return columns;
+            }
+        }
+
+        public int getNumberOfColumnsFriends()
+        {
+            int columns = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT Count(*) FROM information_schema.columns WHERE TABLE_NAME = 'Friends'", connection);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            columns = Int32.Parse(reader[0].ToString());
+                        }
+                    }
+                    finally
+                    {
+                        // Always call Close when done reading.
+                        reader.Close();
+                    }
+                }
+                return columns;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return columns;
+            }
+        }
+
+        public List<string> getFriendsList(string username)
+        {
+            List<string> values = new List<string>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT FriendUsername FROM Friends WHERE Username = @Username", connection);
+
+                    command.Parameters.Add("@Username", SqlDbType.NVarChar, 50);
+                    command.Parameters["@Username"].Value = username;
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        try
+                        {
+                            while (reader.Read())
+                            {
+                                values.Add(reader.GetString(0));
+                            }
+                        }
+                        finally
+                        {
+                            // Always call Close when done reading.
+                            reader.Close();
+                        }
+                    }
+                }
+                return values;
+            }
+            catch (Exception e)
+            {
+                return values;
             }
         }
     }
--- Server/Program.cs
+++ Server/Program.cs
@@ -12,10 +12,23 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            
             DatabaseWorker test = new DatabaseWorker();
+            test.ClearUsersTable();
             User user1 = new User();
             User user2 = new User();
+            user1.Username = "HEY";
+            user1.Password = "HELLO";
+            user2.Username = "Mac";
+            test.AddUser(user1);
+            
             test.AddFriend(user1, user2);
+            test.getFriendsList(user1.Username);
+            List<string> temp = test.getFriendsList(user1.Username);
+            foreach (var a in temp)
+            {
+                Console.WriteLine(a);
+            }
         }
     }
 
