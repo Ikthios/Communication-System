d3dafa628bbc65daf2ce56e321fce061b40b4c1a
Erick <erick.rhen@ndsu.edu>
2015-11-24 11:12:58 -0600

    Getting untracked files to come with

---
+++ Server/LoginHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class LoginHandler
+    {
+        DatabaseWorker worker = new DatabaseWorker();
+        private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Encoding encoding = Encoding.ASCII; 
+
+        public LoginHandler(Socket client)
+        {
+            clientSocket = client;
+        }
+
+        public void StartHandling()
+        {
+            Thread handler = new Thread(loginRequest);
+            handler.Start();
+        }
+
+        public void loginRequest()
+        {
+            string request = "";
+
+            int byteCount;
+            byte[] incoming = new byte[1500];
+            byteCount = clientSocket.Receive(incoming);
+
+            request = encoding.GetString(incoming);
+
+            string[] credentials = request.Split(',');
+
+            bool success = worker.login(credentials[0], credentials[1]);
+
+            if(success)
+            {
+                SendUserInfo(credentials[0]);
+            }
+            else
+            {
+                string failed = "Failed";
+                byte[] message = encoding.GetBytes(failed);
+
+                byteCount = clientSocket.Send(message);
+                clientSocket.Close();
+            }
+        }
+
+        public void SendUserInfo(string username)
+        {
+            int bytecount;
+            List<String> values = worker.getUsernameInfo(username);
+            string replyString = "";
+
+            foreach(var item in values)
+            {
+                replyString += item + ",";
+            }
+
+            replyString.TrimEnd(',');
+
+            byte[] message = encoding.GetBytes(replyString);
+
+            bytecount = clientSocket.Send(message);
+            clientSocket.Close();
+        }
+    }
+}
---
+++ Server/RegistrationHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class RegistrationHandler
+    {
+        DatabaseWorker worker = new DatabaseWorker();
+        private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Encoding encoding = Encoding.ASCII;
+
+        public RegistrationHandler(Socket client)
+        {
+            clientSocket = client;
+        }
+
+        public void StartHandling()
+        {
+            Thread handler = new Thread(RegistrationRequest);
+            handler.Start();
+        }
+
+        public void RegistrationRequest()
+        {
+            string request = "";
+
+            int byteCount;
+            byte[] incoming = new byte[1500];
+            byteCount = clientSocket.Receive(incoming);
+
+            request = encoding.GetString(incoming);
+
+            string[] userInfo = request.Split(',');
+
+            List<String> values = worker.getUsernameInfo(userInfo[0]);
+
+            if(values.Count == 0)
+            {
+                addUser(userInfo);
+            }
+            else
+            {
+                string failed = "Unable to complete registration - Username taken";
+                byte[] message = encoding.GetBytes(failed);
+
+                byteCount = clientSocket.Send(message);
+                clientSocket.Close();
+            }
+        }
+
+        public void addUser(string[] info)
+        {
+            int byteCount;
+            User temp = new User(info);
+            worker.AddUser(temp);
+
+            string success = "Success";
+            byte[] message = encoding.GetBytes(success);
+
+            byteCount = clientSocket.Send(message);
+
+            clientSocket.Close();
+        }
+    }
+}
